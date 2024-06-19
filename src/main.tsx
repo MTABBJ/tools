528f802bb9d1a50b15a063502adef77cdde5146d
moto <13612096789@163.com>
2024-06-19 20:04:47 +0800

    0619-3

--- src/main.tsx
+++ src/main.tsx
@@ -4,16 +4,15 @@ import './index.css'
 import Oml2d from './components/Oml2d'
 import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import store from './store';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   // <React.StrictMode>
   //   <App />
   // </React.StrictMode>,
-  <Provider store={store}>
+  <>
     <Router>
       <App />
     </Router>
-    <Oml2d primaryColor='pink'></Oml2d>
-  </Provider>
+    {/* <Oml2d primaryColor='pink'></Oml2d> */}
+  </>
 )
