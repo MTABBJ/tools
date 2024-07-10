ed5d9ec7277c6bbaca261664da304eb3858aed08
moto <13612096789@163.com>
2024-07-10 22:37:41 +0800

    update

---
+++ src/pages/Home/const.tsx
@@ -0,0 +1,6 @@
+import { describe } from "node:test";
+
+export const staticData = [
+    { title: "指名手配文案生成ツール", description: "にじGTA警察署専用、指名手配の文案を生成するツールです。", path: "/smth" },
+    { title: "在线节拍器", description: "打拍子的测量单位称为 BPM（每分钟拍数），拍速标为 60 BPM 等于分秒打一拍，而 120 BPM 等于每秒打两拍。", path: "/metronome" },
+];
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -5,10 +5,15 @@ import {
   Row, Col, Flex, Typography
 } from 'antd';
 import { localPath } from '@/const'
-import nijiGTA from '@/assets/nijiGTA.jpg'
-import metronome from '@/assets/metronome.webp'
+import { staticData } from './const'
 const { Title, Paragraph, Text } = Typography;
+const { Meta } = Card;
 
+const gridStyle: React.CSSProperties = {
+  width: '25%',
+  textAlign: 'center',
+  background: '#fff',
+};
 const cardStyles = { body: { padding: 0, overflow: 'hidden', height: '13rem' }, }
 const cardStyle: React.CSSProperties = {
   background: '#fff',
@@ -26,56 +31,22 @@ const App: React.FC = () => {
   return (
     <div className='home-page'>
       <Card title="应急工具站" style={{ width: '90vw', height: '90vh' }} >
-        <Row gutter={[16, 16]}>
-          
-          <Col span={12}>
-            <Card hoverable styles={cardStyles} style={cardStyle}>
-              <Flex justify="space-between">
-                <img
-                  alt="avatar"
-                  src={nijiGTA}
-                  style={imgStyle}
-                />
-                <Flex vertical align="flex-end" justify="space-between" style={{ padding: 32 }}>
-                  <Typography>
-                    <Title level={3}>指名手配文案生成ツール</Title>
-                    <Paragraph>
-                      にじGTA警察署専用、指名手配の文案を生成するツールです。
-                    </Paragraph>
-                  </Typography>
-                  <Link to={`${localPath}/smth`}>
-                    <Button type="primary"> 点击前往 </Button>
-                  </Link>
-                </Flex>
-              </Flex>
-            </Card>
-          </Col>
+        <Card>
+          {staticData.map((item) => (
+            <Card.Grid style={gridStyle}>
+              <Typography>
+                <Title level={3}>{item.title}</Title>
+                <Paragraph>
+                  {item.description}
+                </Paragraph>
+              </Typography>
+              <Link to={`${localPath + item.path}`}>
+                <Button type="primary"> 点击前往 </Button>
+              </Link>
+            </Card.Grid>
+          ))}
+        </Card>
 
-          <Col span={12}>
-            <Card hoverable styles={cardStyles} style={cardStyle}>
-              <Flex justify="space-between">
-                <img
-                  alt="avatar"
-                  src={metronome}
-                  style={{ ...imgStyle, margin: '2rem', width: '10rem' }}
-                />
-                <Flex vertical align="flex-end" justify="space-between" style={{ padding: 32 }}>
-                  <Typography>
-                    <Title level={3}>在线节拍器</Title>
-                    <Paragraph>
-                      打拍子的测量单位称为 BPM（每分钟拍数），拍速标为 60 BPM 等于分秒打一拍，而 120 BPM 等于每秒打两拍。
-                    </Paragraph>
-                  </Typography>
-                  <Link to={`${localPath}/metronome`}>
-                    <Button type="primary">
-                      点击前往
-                    </Button>
-                  </Link>
-                </Flex>
-              </Flex>
-            </Card>
-          </Col>
-        </Row>
       </Card>
     </div>
   );
